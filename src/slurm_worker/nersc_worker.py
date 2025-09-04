23d7f03d20e715e5bcb175e3782c0ba2e1168c25
Samuel Hinton <samuelreay@gmail.com>
2025-09-04 17:27:57 +1000

    revert

--- src/slurm_worker/nersc_worker.py
+++ src/slurm_worker/nersc_worker.py
@@ -94,12 +94,9 @@ class NerscWorkerConfiguration(BaseSettings):
         "See https://docs.nersc.gov/jobs/best-practices/#limit-your-queries-to-the-batch-system",
         # Note try to use sacct https://docs.nersc.gov/jobs/monitoring/#sacct for monitoring jobs
     )
-    sfapi_id: str = Field(description="SFAPI client ID", min_length=5)
+    sfapi_id: str = Field(default="", description="SFAPI client ID")
     sfapi_secret: str = Field(
-        description="SFAPI client secret",
-        exclude=True,
-        examples=['{"kty": "RSA", "n": ...}'],
-        min_length=20,
+        default="", description="SFAPI client secret", exclude=True, examples=['{"kty": "RSA", "n": ...}']
     )
 
     model_config = SettingsConfigDict(case_sensitive=False)
